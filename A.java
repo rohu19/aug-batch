bc0c569de3ce71a2b7a7b462e7f16fc32871b1cf
Rohan Kumar <96328617+rohu19@users.noreply.github.com>
2022-09-14 18:41:51 +0530

    my five uploaded file
    mat dekhana

---
+++ A.java
@@ -0,0 +1,14 @@
+
+class A 
+{
+	public static void main(String[] args) 
+	{
+
+		System.out.println("main begin");
+		for(int i=10;i>=3;i--)
+		{
+			System.out.println("loop body:"+i);
+	}
+	System.out.println("main end");
+	}
+}
